01fe1463e5a91a33c6f6787b08196519c793be23
Jeroen Broks <11202073+Tricky1975@users.noreply.github.com>
2024-10-08 12:22:21 +0200

    💀

--- Lunatic.cpp
+++ Lunatic.cpp
@@ -1,7 +1,7 @@
 // Lic:
 // Lunatic/Lunatic.cpp
 // Lunatic
-// version: 23.01.04
+// version: 23.01.17
 // Copyright (C) 2023 Jeroen P. Broks
 // This software is provided 'as-is', without any express or implied
 // warranty.  In no event will the authors be held liable for any damages
@@ -18,18 +18,32 @@
 // 3. This notice may not be removed or altered from any source distribution.
 // EndLic
 
+#undef Lunatic_Debug
+
 #include <iostream>
+#include <SlyvString.hpp>
 #include "Lunatic.hpp"
+
+using namespace Slyvina::Units;
 
 namespace Slyvina {
 	namespace Lunatic {
 
 		lua_CFunction _Lunatic::Panick{ nullptr };
 		std::map<std::string, lua_CFunction> _Lunatic::RegFunction{};
+		uint64 _Lunatic::count{ 0 };
 
-		void _Lunatic::Register(std::string name, lua_CFunction func, bool force) {
+		void _Lunatic::Register4All(std::string name, lua_CFunction func, bool force) {
 			if (RegFunction.count(name)) if (!force) { std::cout << "Dupe function register: " << name << "\tRequest ignored!\n"; return; }
 			RegFunction[name] = func;
+		}
+
+		void _Lunatic::Register(std::string FuncName, lua_CFunction FuncFunc) {
+			lua_register(_State, FuncName.c_str(), FuncFunc);
+		}
+
+		void _Lunatic::Register(std::map<std::string, lua_CFunction> Stuff) {
+			for (auto Reg : Stuff) Register(Reg.first, Reg.second);
 		}
 
 		void _Lunatic::QDoString(std::string source) {
@@ -38,22 +52,68 @@ namespace Slyvina {
 			lua_call(_State, 0, 0);
 		}
 
+		void _Lunatic::QDoByteCode(char* buf, size_t len, std::string chunk) {
+			if (!_State) { std::cout << "Lunatic: Operation cancelled! State is NULL\n"; return; }
+			if (!chunk.size()) chunk = "<Nameless Chunk>";
+			luaL_loadbuffer(_State, buf, len, chunk.c_str());
+			lua_call(_State, 0, 0);
+		}
+
+		void _Lunatic::QDoByteCode(Units::Bank buf, std::string chunk) {
+			if (buf->Expandable()) throw std::runtime_error("QByteCode can NOT be used with expandable banks!");
+			QDoByteCode(buf->Direct(), buf->Size(), chunk);
+		}
+
+		bool _Lunatic::Flag(std::string fl) {
+			Trans2Upper(fl);
+			if (!_Flags.count(fl))	return false;
+			return _Flags[fl];
+		}
+
+		void _Lunatic::Flag(std::string fl, bool v) {
+			Trans2Upper(fl);
+			_Flags[fl] = v;
+		}
+
+
 		_Lunatic::_Lunatic() {
 			auto L{ luaL_newstate() };
 			_State = L;
 			if (Panick) lua_atpanic(L, Panick);
 			luaL_openlibs(L);
+			Register(RegFunction);
+			ID = count++;
+#ifdef Lunatic_Debug
+			std::cout << ID << ":Created Lunatic state: " << (unsigned long long)_State << "/" << (uint64)this << "\n";
+#endif
 		}
 
 		_Lunatic::~_Lunatic() {
-			if (_State) lua_close(_State);
-			_State = nullptr;
+			Kill();
 		}
-		
-		Lunatic LunaticBySource(std::string source) {			
+		void _Lunatic::Kill() {		
+#ifdef Lunatic_Debug
+			std::cout << ID << ":Releasing(" << Released << ") Lunatic state : " << (unsigned long long)_State << " / " << (uint64)this << "\n";
+#endif
+			if (_State && (!Released)) { lua_close(_State); Released = true; }
+			_State = nullptr;
+			Released = true;
+		}
+
+		Lunatic LunaticBySource(std::string source) {
 			auto ret{ std::make_shared<_Lunatic>() };
 			ret->QDoString(source);
 			return ret;
+		}
+
+		Lunatic LunaticByByteCode(char* buf, size_t len, std::string chunk) {
+			auto ret{ std::make_shared<_Lunatic>() };
+			ret->QDoByteCode(buf, len, chunk);
+			return ret;
+		}
+
+		Lunatic LunaticByByteCode(Slyvina::Units::Bank buf, std::string chunk) {
+			return LunaticByByteCode(buf->Direct(), buf->Size(), chunk);
 		}
 	}
 
--- Lunatic.hpp
+++ Lunatic.hpp
@@ -1,8 +1,8 @@
 // Lic:
 // Lunatic/Lunatic.hpp
 // Lunatic (header)
-// version: 23.01.04
-// Copyright (C) 2022, 2023 Jeroen P. Broks
+// version: 24.10.08
+// Copyright (C) 2022, 2023, 2024 Jeroen P. Broks
 // This software is provided 'as-is', without any express or implied
 // warranty.  In no event will the authors be held liable for any damages
 // arising from the use of this software.
@@ -17,6 +17,9 @@
 // misrepresented as being the original software.
 // 3. This notice may not be removed or altered from any source distribution.
 // EndLic
+
+#pragma once
+
 #pragma region Lua
 extern "C" {
 #include "Lua/Raw/src/lua.h"
@@ -32,28 +35,64 @@ extern "C" {
 #include <string>
 #pragma endregion
 
+#pragma region "Slyvina includes"
+#include <SlyvBank.hpp>
+#pragma endregion
+
 namespace Slyvina {
 
-	namespace Lunatic {
+	namespace NSLunatic {
 
 		class _Lunatic;
 
-		typedef std::shared_ptr<_Lunatic> Lunatic;
+		typedef std::shared_ptr<_Lunatic> Lunatic; // deprecated
+		typedef std::shared_ptr<_Lunatic> SLunatic;
 		typedef std::unique_ptr<_Lunatic> ULunatic;
+
+		typedef std::map<std::string, lua_CFunction> LunaticRegMap;
+
+		struct _LunDbg {
+			std::string File{ "" };
+			std::string Func{ "" };
+			size_t Line{ 0 };
+		};
+
+
 
 		class _Lunatic {
 		private:
+			static uint64 count;
+			uint64 ID;
 			lua_State* _State{ nullptr };
 			static std::map<std::string, lua_CFunction> RegFunction;
+			std::map < std::string, bool> _Flags{};
+			bool Released{ false };
 		public:
+			bool Debug{ false };
+			std::vector<_LunDbg> Trace{};
+
 			static lua_CFunction Panick; 
 			inline lua_State* State() { return _State; } // This makes the state not directly writeable from outside the class.
 			inline static std::string Lua_Version() { return std::string(LUA_VERSION_MAJOR) + "." + std::string(LUA_VERSION_MINOR) + "." + std::string(LUA_VERSION_RELEASE); }
 			
-			void Register(std::string name, lua_CFunction func, bool force = false);
-			inline void Register(std::map<std::string, lua_CFunction> Stuff) { for (auto& St : Stuff) Register(St.first, St.second); }
+
+			static void Register4All(std::string name, lua_CFunction func, bool force = false);
+			static inline void Register4All(std::map<std::string, lua_CFunction> Stuff) { for (auto& St : Stuff) Register4All(St.first, St.second); }
+
+			void Register(std::string FuncName, lua_CFunction FuncFunc);
+			void Register(std::map<std::string, lua_CFunction> Stuff);
 
 			void QDoString(std::string source);
+			void QDoByteCode(char* buf, size_t len,std::string chunk="");
+			void QDoByteCode(Units::Bank buf, std::string chunk = "");
+
+			bool Flag(std::string fl);
+			void Flag(std::string fl, bool v);
+
+			/// <summary>
+			/// Kills the Lua state within. Please note, this renders this object unusable. Normally this happens automatically when the Lunatic object is destroyed.
+			/// </summary>
+			void Kill(); 
 
 			_Lunatic();
 			~_Lunatic();
@@ -61,6 +100,20 @@ namespace Slyvina {
 		};
 
 		Lunatic LunaticBySource(std::string source);
+		Lunatic LunaticByByteCode(char* buf,size_t len,std::string chunk="");
+		Lunatic LunaticByByteCode(Units::Bank buf, std::string chunk = "");
+		
+		/// <summary>
+		/// Quick and dirty function which will return a C++ from a luaL_checkstring in stead of a const char*.
+		/// </summary>
+		/// <param name="L">The Lua State</param>
+		/// <param name="pos">Position</param>
+		/// <returns>The wanted string</returns>
+		inline std::string Lunatic_CheckString(lua_State* L, int pos) { return luaL_checkstring(L, pos); }
+		
+		inline std::string Lunatic_OptString(lua_State* L, int pos, std::string defaultvalue) { return luaL_optstring(L, pos, defaultvalue.c_str()); }
+
+		inline void Lunatic_PushString(lua_State* L, std::string str) { lua_pushstring(L, str.c_str()); }
 		
 
 	}
